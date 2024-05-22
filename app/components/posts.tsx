4fe6d4ecdc786d0904044eb51b758eb0ded5033d
Arsen Klyuev <arsenklyuev@Arsens-MacBook-Pro.local>
2024-05-22 15:35:30 -0500

    Working on first blog post

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -15,22 +15,26 @@ export function BlogPosts() {
           }
           return 1;
         })
-        .map((post) => (
-          <Link
-            key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
-            href={`/blog/${post.slug}`}
-          >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
-                {formatDate(post.metadata.publishedAt, false)}
-              </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                {post.metadata.title}
-              </p>
-            </div>
-          </Link>
-        ))}
+        .map(
+          (post) =>
+            !post.metadata.hidden && (
+              <Link
+                key={post.slug}
+                className="flex flex-col space-y-1 mb-4"
+                href={`/blog/${post.slug}`}
+              >
+                <p>{post.metadata.hidden}</p>
+                <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+                  <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
+                    {formatDate(post.metadata.publishedAt, false)}
+                  </p>
+                  <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+                    {post.metadata.title}
+                  </p>
+                </div>
+              </Link>
+            ),
+        )}
     </div>
   );
 }
